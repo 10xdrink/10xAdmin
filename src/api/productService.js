1da3f2aa0866c12e1e39aaf914429b1c1eaf5357
10xdrink <it@10xdrink.com>
2025-02-13 13:32:33 +0530

    Update productService.js

--- src/api/productService.js
+++ src/api/productService.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 // Base URL from environment variables
-const API_URL = import.meta.env.VITE_API_URL || 'http://3.110.92.192:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://backend.10xdrink.com/api';
 
 // Create an Axios instance for products
 const axiosInstance = axios.create({
