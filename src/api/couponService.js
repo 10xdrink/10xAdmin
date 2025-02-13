a105fc129633947d32ac349883fb5c2015943b67
10xdrink <it@10xdrink.com>
2025-02-13 13:31:57 +0530

    Update couponService.js

--- src/api/couponService.js
+++ src/api/couponService.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://3.110.92.192:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://backend.10xdrink.com/api';
 
 // Create an axios instance for coupons
 const axiosInstance = axios.create({
